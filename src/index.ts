d625992c70e4f2d2a3754c49ccb00963cc7ce1d9
Zsolt Gömböcz <zsolt.gombocz00@gmail.com>
2023-06-28 18:57:06 +0200

    Basic express app entry created

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,21 @@
-console.log('Hello world!')
+import express, { Application, Request, Response } from "express";
+import routes from './routes';
+import { client } from "./db";
+import { getFilteredStocks } from "./scrapeStocks";
+
+const app: Application = express();
+
+app.use('/', routes);
+
+const PORT = 3000;
+
+try {
+    app.listen(PORT, (): void => {
+        console.log(`Connected successfully on port ${PORT}`);
+
+        client.connect();
+    });
+
+} catch (error: any) {
+    console.error(`Error occured: ${error.message}`);
+}
