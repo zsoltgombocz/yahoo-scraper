50e9f95be91c3d73c38eaea8855532baa26101a3
Zsolt Gömböcz <zsolt.gombocz00@gmail.com>
2023-06-29 20:22:53 +0200

    added finance route to start the process

--- src/routes.ts
+++ src/routes.ts
@@ -1,7 +1,8 @@
 import express, { Response, Request, Router } from "express";
 import { STATE, globalState } from "./state";
-import { getFilteredStocks } from "./scrapeStocks";
 import { client } from "./db";
+import { saveFilteredStocks } from "./scrapeStocks";
+import { saveFinancialData } from "./scrapeFinance";
 
 const router: Router = express.Router();
 
@@ -10,25 +11,40 @@ router.get("/", async (req: Request, res: Response): Promise<Response> => {
 });
 
 router.get('/status', async (req: Request, res: Response): Promise<Response> => {
-    const finvizLast = await client.get('finviz_last')
+    const finvizLast = await client.get('finviz_last');
     const finvizLastUnix = parseInt(finvizLast || '0', 10);
     const finvizDate = new Date(finvizLastUnix);
-    console.log(finvizDate);
+
+    const financeLast = await client.get('finance_last');
+    const financeLastUnix = parseInt(financeLast || '0', 10);
+    const financeDate = new Date(financeLastUnix);
 
     return res.status(200).json({
         'finviz': {
             'status': globalState.stock,
             'last_fetch': finvizDate.toLocaleString(),
+        },
+        'finance': {
+            'status': globalState.finance,
+            'last_fetch': financeDate.toLocaleString(),
         }
     });
 })
 
-router.get('/fetch', async (req: Request, res: Response): Promise<Response> => {
+router.get('/stock', async (req: Request, res: Response): Promise<Response> => {
     if (globalState.stock !== STATE.DOING) {
-        getFilteredStocks();
+        saveFilteredStocks();
     }
 
     return res.status(200).send(`${globalState.stock}. A "/status" oldalon lehet látni az állapotot.`);
+})
+
+router.get('/finance', async (req: Request, res: Response): Promise<Response> => {
+    if (globalState.finance !== STATE.DOING) {
+        saveFinancialData();
+    }
+
+    return res.status(200).send(`${globalState.finance}. A "/status" oldalon lehet látni az állapotot.`);
 })
 
 export default router;
