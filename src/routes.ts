8fb4edcd76363209a730263f1461b84f7dd73955
Zsolt Gömböcz <zsolt.gombocz00@gmail.com>
2023-07-02 14:44:25 +0200

    Added route to get info from stocks + status simplified

--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,6 @@
 import express, { Response, Request, Router } from "express";
 import { STATE, globalState } from "./state";
-import { client } from "./db";
+import { client, fetchType, getStock } from "./db";
 import { saveFilteredStocks } from "./scrapeStocks";
 import { saveFinancialData } from "./scrapeFinance";
 
@@ -11,27 +11,22 @@ router.get("/", async (req: Request, res: Response): Promise<Response> => {
 });
 
 router.get('/status', async (req: Request, res: Response): Promise<Response> => {
-    const finvizLast = await client.get('finviz_last');
-    const finvizLastUnix = parseInt(finvizLast || '0', 10);
-    const finvizDate = new Date(finvizLastUnix);
-
-    const financeLast = await client.get('finance_last');
-    const financeLastUnix = parseInt(financeLast || '0', 10);
-    const financeDate = new Date(financeLastUnix);
+    const finviz = await client.get(`${fetchType.FINVIZ}_last`);
+    const yahoo = await client.get(`${fetchType.YAHOO}_last`);
 
     return res.status(200).json({
-        'finviz': {
+        [fetchType.FINVIZ]: {
             'status': globalState.stock,
-            'last_fetch': finvizDate.toLocaleString(),
+            'last_fetch': finviz ? new Date(parseInt(finviz || '0')).toLocaleString() : null,
         },
-        'finance': {
+        [fetchType.YAHOO]: {
             'status': globalState.finance,
-            'last_fetch': financeDate.toLocaleString(),
+            'last_fetch': yahoo ? new Date(parseInt(yahoo || '0')).toLocaleString() : null,
         }
     });
 })
 
-router.get('/stock', async (req: Request, res: Response): Promise<Response> => {
+router.get('/fetch/stock', async (req: Request, res: Response): Promise<Response> => {
     if (globalState.stock !== STATE.DOING) {
         saveFilteredStocks();
     }
@@ -39,12 +34,18 @@ router.get('/stock', async (req: Request, res: Response): Promise<Response> => {
     return res.status(200).send(`${globalState.stock}. A "/status" oldalon lehet látni az állapotot.`);
 })
 
-router.get('/finance', async (req: Request, res: Response): Promise<Response> => {
+router.get('/fetch/finance', async (req: Request, res: Response): Promise<Response> => {
     if (globalState.finance !== STATE.DOING) {
         saveFinancialData();
     }
 
     return res.status(200).send(`${globalState.finance}. A "/status" oldalon lehet látni az állapotot.`);
+});
+
+router.get('/info/:stock', async (req: Request, res: Response): Promise<Response> => {
+    const stockInfo = await getStock(req.params.stock);
+
+    return res.status(200).json(stockInfo);
 })
 
 export default router;
