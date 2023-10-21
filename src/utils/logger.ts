24e73d42fbc01fe4d22072478be6491c5a6a1a5a
Zsolt Gömböcz <zsolt.gombocz00@gmail.com>
2023-10-21 16:49:44 +0200

    make logger log to console when not in production

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,8 +1,26 @@
+import 'dotenv/config';
 const winston = require("winston");
 
 const customFormat: string = winston.format.printf((data: any) => {
   return `${data.timestamp} - ${data.message}`;
 });
+
+const getTransports = (): any[] => {
+  const transports = [];
+
+  transports.push(
+    new winston.transports.File({
+      filename: 'log/info.log',
+      level: 'info'
+    })
+  );
+
+  if (process.env.NODE_ENV !== 'production') {
+    transports.push(new winston.transports.Console());
+  }
+
+  return transports;
+}
 
 export const logger = winston.createLogger({
   level: "debug",
@@ -10,10 +28,5 @@ export const logger = winston.createLogger({
     winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
     customFormat
   ),
-  transports: [
-    new winston.transports.File({
-      filename: 'log/info.log',
-      level: 'info'
-    })
-  ],
+  transports: getTransports(),
 });
