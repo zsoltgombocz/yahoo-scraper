4a9278947eb9dbe331a73ea9141335a95cca8df9
Zsolt Gömböcz <zsolt.gombocz00@gmail.com>
2023-10-21 16:54:32 +0200

    ServiceState wrapper class

---
+++ src/utils/ServiceState.ts
@@ -0,0 +1,29 @@
+
+import { client } from "../redis/client";
+import { scraperStatus } from "../types";
+
+export type scraperState = {
+    timestamp: number;
+    state: scraperStatus;
+}
+
+export class ServiceState {
+    signature: string;
+
+    constructor(signature: string) {
+        this.signature = signature;
+    }
+
+    get = async (): Promise<scraperState | null> => {
+        const data: string | null = await client.get(this.signature);
+
+        if (data === null) return null;
+
+        return JSON.parse(data) as scraperState;
+    }
+
+    //? Do not need to wait for anything this can happen in the background
+    save = (status: scraperStatus): void => {
+        client.set(this.signature, JSON.stringify({ status, timestamp: new Date().getTime() }));
+    }
+}
