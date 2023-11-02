4f735b047780a8c365667e86398e2431910a9b24
Zsolt Gömböcz <zsolt.gombocz00@gmail.com>
2023-11-02 16:24:56 +0100

    removed unused imports

--- src/routes.ts
+++ src/routes.ts
@@ -1,15 +1,6 @@
 import express, { Response, Request, Router } from "express";
-import { STATE, globalState } from "./state";
-import { saveFilteredStocks } from "./scrapeStocks";
-import { saveFinancialData } from "./scrapeFinance";
-import { checkStocks, getIncomePercentage, getTypesBasedOnEligibility, isEligible } from "./filterStocks";
-import ExcelJS from 'exceljs';
-import MemoryStream from 'memorystream';
-import { generateExcel } from "./generateExcel";
-import { runMonthlyCheck } from "./monthlyCheck";
 import Stock from "./Stock";
 import { ServiceState } from "./utils/ServiceState";
-import { scraperStatus } from "./types";
 
 const router: Router = express.Router();
 
