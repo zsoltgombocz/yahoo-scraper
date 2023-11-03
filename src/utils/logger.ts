695927aee7a86aa98c18f9b9f9c29c2039f1fd79
Zsolt Gömböcz <zsolt.gombocz00@gmail.com>
2023-11-03 18:45:19 +0100

    log to console when not in prod

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -5,28 +5,18 @@ const customFormat: string = winston.format.printf((data: any) => {
   return `${data.timestamp} - ${data.message}`;
 });
 
-const getTransports = (): any[] => {
-  const transports = [];
-
-  transports.push(
-    new winston.transports.File({
-      filename: 'log/info.log',
-      level: 'info'
-    })
-  );
-
-  if (process.env.NODE_ENV !== 'production') {
-    transports.push(new winston.transports.Console());
-  }
-
-  return transports;
-}
-
 export const logger = winston.createLogger({
   level: "debug",
   format: winston.format.combine(
     winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
     customFormat
   ),
-  transports: getTransports(),
+  transports: [
+    new winston.transports.File({
+      filename: 'log/info.log',
+      level: 'info'
+    }),
+
+    new winston.transports.Console()
+  ]
 });
