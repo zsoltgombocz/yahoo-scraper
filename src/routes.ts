ce748d7a499c1fa81996554adeed390b8d8b41ed
Zsolt Gömböcz <zsolt.gombocz00@gmail.com>
2023-11-07 17:06:35 +0100

    add excel generation when hitting a route

--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,9 @@
 import express, { Response, Request, Router } from "express";
 import Stock from "./Stock";
 import { ServiceState } from "./utils/ServiceState";
+import ServiceWrapper from "./ServiceWrapper";
+import FinvizService from "./services/FinvizService";
+import YahooService from "./services/YahooService";
 
 const router: Router = express.Router();
 
@@ -27,6 +30,16 @@ router.get("/stock/:name", async (req: Request, res: Response): Promise<Response
 router.get('/download/:year/:month', async (req: Request, res: Response): Promise<any> => {
     const year = req.params.year;
     const month = req.params.month;
+
+    return res.download(`${process.cwd()}/public/${year}-${month}.xlsx`);
+});
+
+router.get('/generate', async (req: Request, res: Response): Promise<any> => {
+    const serviceWrapper = new ServiceWrapper({} as FinvizService, {} as YahooService);
+    await serviceWrapper.generateExcel();
+
+    const year = new Date().getFullYear();
+    const month = new Date().getMonth() + 1;
 
     return res.download(`${process.cwd()}/public/${year}-${month}.xlsx`);
 });
